d414bd52970ef7c9ac8a736c30e4671512be0129
andria-goodwin <143760838+andria-goodwin@users.noreply.github.com>
2024-02-16 19:03:47 -0600

    added css framework and about me page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,30 @@
+import '../App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Image, Row, Col } from 'react-bootstrap'
+
+const styles = {
+  about: {
+    textAlign: 'center',
+  },
+  img: {
+    width: 250,
+    height: 250,
+  },
+};
+
 function About() {
   return (
-    <div>About</div>
+    <div style={styles.about}>
+      <Row>
+        <Col md>
+          <Image src='src/assets/AG-photo.jpg' style={styles.img} roundedCircle />
+        </Col>
+        <Col md>
+          <h2>My name is Andria Goodwin</h2>
+          <p>I am a coding bootcamp student through KU and edX. I know that although I have learned a lot during my time in coding bootcamp, there will always be more to learn and ways to become more efficient. My goal is to always be open to learning and growing.</p>
+        </Col>
+      </Row>  
+    </div>
   )
 }
 
